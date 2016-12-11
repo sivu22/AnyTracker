0f80a3b69d95b746f0bf1e6dfd71979adfc5e127
Cristian Sava <cristianzsava@gmail.com>
2016-12-11 14:51:21 +0100

    Fix Items reorder bug

--- AnyTracker/ItemsTableViewController.swift
+++ AnyTracker/ItemsTableViewController.swift
@@ -364,6 +364,7 @@ extension ItemsTableViewController {
     
     override func positionChanged(currentIndex: IndexPath, newIndex: IndexPath) {
         list!.exchangeItem(fromIndex: currentIndex.row, toIndex: newIndex.row)
+        swap(&items[currentIndex.row], &items[newIndex.row])
     }
     
     override func reorderFinished(initialIndex: IndexPath, finalIndex: IndexPath) {
